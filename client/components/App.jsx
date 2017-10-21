1f3c8adaae3e99120f11d212406464f14a9a4705
Christopher Washburn <chris132128@gmail.com>
2017-10-20 19:37:33 -0700

    ready to go

---
+++ client/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Main from './Main.jsx';
+
+class App extends React.Component {
+  render() {
+    return(
+      <div id="app-container">
+        <Main />
+      </div>
+    );
+  }
+}
+
+export default App;

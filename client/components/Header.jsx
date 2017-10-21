25a2ff688b897684e796695229ad3dda7ad11275
Christopher Washburn <chris132128@gmail.com>
2017-10-20 20:54:32 -0700

    added header and search bar

---
+++ client/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+class Header extends React.Component {
+  render() {
+    return(
+      <div id="sign-in container">
+        <div id="logo-container">
+          <img alt="logo"/>
+        </div>
+        <div>
+          <h1 id="title"></h1>
+        </div>
+        <div id="sign-in-container">
+          <button>Sign In</button>
+          <button>Sign Up</button>
+          <button>Logout</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import Header from './Header';
+import Search from './Search';
+
 class Home extends React.Component {
   render() {
     return(
       <div id="home-container">
-        Hello World
+        <Header />
+        <Search />
       </div>
     );
   }
